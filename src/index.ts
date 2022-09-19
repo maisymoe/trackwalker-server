dfce549f24a5bc1838c3532c02e3a6662f5d9b1a
Beef <beefers@riseup.net>
2022-09-19 20:07:56 +0100

    [Global] Restructure

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 // Imports
 import { Server } from "socket.io";
 
+// Variables
+const port = parseInt(process.env.TRACKWALKER_PORT as string) || 4040;
+
 // Init the server
-const io = new Server<ClientToServerEvents, ServerToClientEvents>(4040, {});
+const io = new Server<ClientToServerEvents, ServerToClientEvents>(port, {});
 
 // Validate usernames
 io.use((socket, next) => {
@@ -11,8 +14,15 @@ io.use((socket, next) => {
 })
 
 // Listen for connections
-io.on("connection", (socket) => {
-    socket.on("updatePosition", (pos: Position) => {
-        socket.broadcast.emit("playerUpdate", socket.handshake.auth.username, pos);
-    });
+io.on("connection", (socket: TrackwalkerSocket) => {
+    socket.player = {
+        username: socket.handshake.auth.username,   
+        id: socket.id,
+    };
+
+    socket.broadcast.emit("playerJoin", socket.player);
+    socket.on("disconnect", () => socket.broadcast.emit("playerLeave", socket.player!));
+    socket.on("positionUpdate", (pos: Position) => socket.broadcast.emit("playerUpdate", socket.player!, pos));
 });
+
+console.log(`Listening on ${port}`);
