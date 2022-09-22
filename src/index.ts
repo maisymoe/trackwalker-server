1326992bd6091b7f6a9fae420ee83abbc332fc8d
Beef <beefers@riseup.net>
2022-09-22 19:33:22 +0100

    [Cleanup] Server-side cleanups!

--- src/index.ts
+++ src/index.ts
@@ -13,34 +13,36 @@ io.use((socket, next) => {
     next();
 })
 
+// Make an array to store active players in
 const players = new Array<Player>();
 
 // Listen for connections
 io.on("connection", (socket: TrackwalkerSocket) => {
+    // Assign the socket a player object
     socket.player = {
         username: socket.handshake.auth.username,   
         id: socket.id,
     };
 
+    // Add the player to the players array, log that they connected, and broadcast their presence
     players.push(socket.player);
-
-    // TODO: Duplicate connections bug
-
     console.log(`${socket.player.username} (${socket.id}) connected`);
     socket.broadcast.emit("playerJoin", socket.player);
 
-    socket.on("disconnect", () => { 
-        socket.broadcast.emit("playerLeave", socket.player!);
+    // When a socket disconnects, remove them from the players array, log that they disconnected and broadcast that they left
+    socket.on("disconnect", () => {
         players.splice(players.indexOf(socket.player!));
         console.log(`${socket.player!.username} (${socket.id}) disconnected`);
+        socket.broadcast.emit("playerLeave", socket.player!);
     });
 
+    // If socket asks for player list, hand it over
+    socket.on("requestPlayers", () => socket.emit("recievePlayers", players));
+
+    // Broadcast any events from the socket to other sockets
     socket.on("positionUpdate", (pos: Position) => socket.broadcast.emit("playerPositionUpdate", socket.player!, pos));
     socket.on("animationUpdate", (anim: string, direction: Vec2) => socket.broadcast.emit("playerAnimationUpdate", socket.player!, anim, direction));
-
-    socket.on("requestPlayers", () => {
-        socket.emit("recievePlayers", players);
-    });
 });
 
+// Log the port
 console.log(`Listening on ${port}`);
